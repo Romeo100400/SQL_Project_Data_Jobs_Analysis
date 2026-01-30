69f8496b002172479bdea86cfb5d69d76ea41768
Romeo100400 <romemattolini@gmail.com>
2026-01-30 16:26:40 +0100

    upload swl files

---
+++ 3_top_demanded_skills.sql
@@ -0,0 +1,27 @@
+/*
+-- Question: What are the most in-demand skills for REMOTE data analysts in the UK?
+ - Join job postings to inner join table similar to query 2
+ - Identify the top 5 in-demand skills for a data analyst.
+ - Focus on job postings only in the United Kingdom.
+ - Why? Retrieves the top 5 skills with the highest demand in these regions,
+        providing insights into valuable skills for job seekers.
+
+*/
+
+SELECT
+    skills,
+    COUNT(DISTINCT skills_job_dim.job_id) AS demand_count     -- count how many times each skill appears
+FROM job_postings_fact
+INNER JOIN skills_job_dim 
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst'                 -- only data analyst roles
+    AND job_country = 'United Kingdom'            --UK focus
+    AND job_work_from_home = TRUE                    -- Only remote jobs
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5;
